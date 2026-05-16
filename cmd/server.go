592baff26bdffd582c7d371bcbd5b8a5d60c88e5
igortuchel <ITuchel@pm.me>
2026-05-16 23:43:29 +0100

    added ttl

--- cmd/server.go
+++ cmd/server.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/igortuchel/go-redis/internal/parser"
 	"github.com/igortuchel/go-redis/internal/store"
@@ -51,6 +53,7 @@ func handleConn(conn net.Conn, s *store.Store) {
 
 		fmt.Println("recieved: ", value)
 
+		// TODO: Refactor into a command dispatcher
 		command := strings.ToUpper(value.Array[0].Bulk)
 		args := value.Array[1:]
 
@@ -59,19 +62,63 @@ func handleConn(conn net.Conn, s *store.Store) {
 			writer.Write(parser.Value{RType: parser.STRING, Str: "PONG"})
 		case "SET":
 			s.Set(args[0].Bulk, args[1].Bulk)
+			if len(args) >= 4 {
+				flag := strings.ToUpper(args[2].Bulk)
+				ttl, err := strconv.Atoi(args[3].Bulk)
+				if err != nil || ttl <= 0 {
+					writer.Write(parser.Value{RType: parser.ERROR, Str: "ERR invalid expire time in 'set' command"})
+					break
+				}
+				switch flag {
+				case "PX":
+					s.Expire(args[0].Bulk, time.Duration(ttl)*time.Millisecond)
+				case "EX":
+					s.Expire(args[0].Bulk, time.Duration(ttl)*time.Second)
+				default:
+					writer.Write(parser.Value{RType: parser.ERROR, Str: "ERR syntax error"})
+					break
+				}
+			}
 			writer.Write(parser.Value{RType: parser.STRING, Str: "OK"})
 		case "GET":
 			val, ok := s.Get(args[0].Bulk)
-			if !ok {
-				writer.Write(parser.Value{RType: parser.NULL})
-			} else {
+			if ok {
 				writer.Write(parser.Value{RType: parser.BULK, Bulk: val})
+			} else {
+				writer.Write(parser.Value{RType: parser.NULL})
+			}
+		case "EXPIRE":
+			ttl, err := strconv.Atoi(args[1].Bulk)
+			if err != nil || ttl <= 0 {
+				writer.Write(parser.Value{RType: parser.INTEGER, Num: 0})
+				break
+			}
+			ok := s.Expire(args[0].Bulk, time.Duration(ttl)*time.Second)
+			if ok {
+				writer.Write(parser.Value{RType: parser.INTEGER, Num: 1})
+			} else {
+				writer.Write(parser.Value{RType: parser.INTEGER, Num: 0})
 			}
 		case "DEL":
-			s.Del(args[0].Bulk)
-			writer.Write(parser.Value{RType: parser.INTEGER, Num: 1})
+			ok := s.Del(args[0].Bulk)
+			if ok {
+				writer.Write(parser.Value{RType: parser.INTEGER, Num: 1})
+			} else {
+				writer.Write(parser.Value{RType: parser.INTEGER, Num: 0})
+			}
+		case "EXISTS":
+			exists := s.Exists(args[0].Bulk)
+			if exists {
+				writer.Write(parser.Value{RType: parser.INTEGER, Num: 1})
+			} else {
+				writer.Write(parser.Value{RType: parser.INTEGER, Num: 0})
+
+			}
 		case "ECHO":
 			writer.Write(args[0])
+		case "TTL":
+			ttl := s.Ttl(args[0].Bulk)
+			writer.Write(parser.Value{RType: parser.INTEGER, Num: ttl})
 		default:
 			writer.Write(parser.Value{RType: parser.ERROR, Str: "ERR unknown command"})
 		}
--- internal/store/store.go
+++ internal/store/store.go
@@ -2,34 +2,92 @@ package store
 
 import (
 	"sync"
+	"time"
 )
 
 type Store struct {
 	mu   sync.RWMutex
-	data map[string]string
+	data map[string]Entry
+}
+
+type Entry struct {
+	Value  string
+	Expiry time.Time
 }
 
 func New() *Store {
 	return &Store{
-		data: make(map[string]string),
+		data: make(map[string]Entry),
 	}
+}
+
+func (s *Store) Expire(key string, ttl time.Duration) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	entry, ok := s.data[key]
+	if !ok {
+		return false
+	}
+	entry.Expiry = time.Now().Add(ttl)
+	s.data[key] = entry
+	return true
 }
 
 func (s *Store) Set(key, value string) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	s.data[key] = value
+	s.data[key] = Entry{Value: value}
 }
 
 func (s *Store) Get(key string) (string, bool) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
-	val, ok := s.data[key]
-	return val, ok
+	entry, ok := s.data[key]
+	if !ok {
+		return "", false
+	}
+
+	if !entry.Expiry.IsZero() && time.Now().After(entry.Expiry) {
+		return "", false
+	}
+	return entry.Value, true
 }
 
-func (s *Store) Del(key string) {
+func (s *Store) Del(key string) bool {
 	s.mu.Lock()
 	defer s.mu.Unlock()
+	_, ok := s.data[key]
 	delete(s.data, key)
+	return ok
+}
+
+func (s *Store) Exists(key string) bool {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	entry, ok := s.data[key]
+	if !ok {
+		return false
+	}
+	if !entry.Expiry.IsZero() && time.Now().After(entry.Expiry) {
+		return false
+	}
+	return true
+}
+
+func (s *Store) Ttl(key string) int {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	entry, ok := s.data[key]
+	if !ok {
+		return -2
+	}
+	if entry.Expiry.IsZero() {
+		return -1
+	}
+
+	if time.Now().After(entry.Expiry) {
+		return -2
+	}
+	return int(time.Until(entry.Expiry).Seconds())
 }
