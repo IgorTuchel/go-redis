66ded4ffc834d5bb000f76343762d8967b77190b
igortuchel <ITuchel@pm.me>
2026-05-16 18:07:48 +0100

    added store

--- cmd/server.go
+++ cmd/server.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"strings"
 
 	"github.com/igortuchel/go-redis/internal/parser"
+	"github.com/igortuchel/go-redis/internal/store"
 )
 
 func main() {
@@ -20,17 +22,18 @@ func main() {
 	defer l.Close()
 
 	fmt.Println("Server running on port :6379")
+	store := store.New()
 
 	for {
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("error accepting connection: ", err.Error())
 		}
-		go handleConn(conn)
+		go handleConn(conn, store)
 	}
 }
 
-func handleConn(conn net.Conn) {
+func handleConn(conn net.Conn, s *store.Store) {
 	defer conn.Close()
 
 	writer := parser.NewWriter(conn)
@@ -48,6 +51,30 @@ func handleConn(conn net.Conn) {
 
 		fmt.Println("recieved: ", value)
 
-		writer.Write(parser.Value{RType: parser.STRING, Str: "OK"})
+		command := strings.ToUpper(value.Array[0].Bulk)
+		args := value.Array[1:]
+
+		switch command {
+		case "PING":
+			writer.Write(parser.Value{RType: parser.STRING, Str: "PONG"})
+		case "SET":
+			s.Set(args[0].Bulk, args[1].Bulk)
+			writer.Write(parser.Value{RType: parser.STRING, Str: "OK"})
+		case "GET":
+			val, ok := s.Get(args[0].Bulk)
+			if !ok {
+				writer.Write(parser.Value{RType: parser.NULL})
+			} else {
+				writer.Write(parser.Value{RType: parser.BULK, Bulk: val})
+			}
+		case "DEL":
+			s.Del(args[0].Bulk)
+			writer.Write(parser.Value{RType: parser.INTEGER, Num: 1})
+		case "ECHO":
+			writer.Write(args[0])
+		default:
+			writer.Write(parser.Value{RType: parser.ERROR, Str: "ERR unknown command"})
+		}
+
 	}
 }
